ac47974f2eefb71ac50a149d4c9d5d9d77a4673a
Kerekes Roland <kerekes98roland@gmail.com>
2022-04-17 19:54:18 +0200

    fixing bug

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,22 +1,37 @@
 import React from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/Cart-Context";
 
 const HeaderCartButton = (props) => {
     const { items } = useContext(CartContext);
+    const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
 
     const numberOfCartItem = items.reduce((current, item) => {
         return current + item.amount;
     }, 0);
+
+    const btnClasses = `${classes.button} ${
+        buttonIsHighlighted ? classes.bump : ""
+    }`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setButtonIsHighlighted(true);
+        setTimeout(() => {
+            setButtonIsHighlighted(false);
+        }, 500);
+    }, [items]);
 
     return (
         <button onClick={props.onClick} className={classes.button}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
-            <span>Your Cart</span>
+            <span className={btnClasses}>Your Cart</span>
             <span className={classes.badge}>{numberOfCartItem}</span>
         </button>
     );
